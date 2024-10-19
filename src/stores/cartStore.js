9e4c4b6a8e590fd20b4094bfbaf11c72d87bda95
Mingjie-Xie <3234126808@qq.com>
2024-10-19 20:46:07 +0800

    本地购物车-购物车列表-统计计算

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -31,12 +31,17 @@ export const useCartStore = defineStore('cart',() =>{
    const allCount =  computed(() =>cartList.value.reduce((a,c) =>a+c.count,0))
    const allPrice =  computed(() =>cartList.value.reduce((a,c) =>a+c.count*c.price,0))
    
+    const selectedCount = computed(() => cartList.value.filter(item => item.selected).reduce((a,c) => a+c.count,0))
+    const selectedPrice  = computed(() => cartList.value.filter(item => item.selected).reduce((a,c) => a+c.count*c.price,0))
+
    const isAll= computed(() => cartList.value.every((item) => item.selected))
    return {
         cartList,
         allCount,
         allPrice,
         isAll,
+        selectedCount,
+        selectedPrice,
         addCart,
         delCart,
         singleCheck,
