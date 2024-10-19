b44adc3c47c0b74841e0a13ffc978b269210a0f1
Mingjie-Xie <3234126808@qq.com>
2024-10-19 20:36:36 +0800

    本地购物车-全选功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -22,17 +22,25 @@ export const useCartStore = defineStore('cart',() =>{
     const item =  cartList.value.find((item) =>item.skuId === skuId)
      item.selected = selected
  }
+
+ const allCheck = (selected) =>{
+    cartList.value.forEach(item => item.selected = selected)
+}
  
 
    const allCount =  computed(() =>cartList.value.reduce((a,c) =>a+c.count,0))
    const allPrice =  computed(() =>cartList.value.reduce((a,c) =>a+c.count*c.price,0))
-    return {
+   
+   const isAll= computed(() => cartList.value.every((item) => item.selected))
+   return {
         cartList,
         allCount,
         allPrice,
+        isAll,
         addCart,
         delCart,
-        singleCheck
+        singleCheck,
+        allCheck
     }
  },{
         persist: true,
