92eb2583460892f5488faf2b3537524ed245e5e8
Mingjie-Xie <3234126808@qq.com>
2024-10-22 19:10:57 +0800

    订单-使用基础参数获取基础列表

--- src/utils/http.js
+++ src/utils/http.js
@@ -7,7 +7,7 @@ import {useUserStore} from '@/stores/userStore'
 
 const httpInstance = axios.create({
     baseURL:'http://pcapi-xiaotuxian-front-devtest.itheima.net',
-    timeout: 5000
+    timeout: 20000
 
 })
 
